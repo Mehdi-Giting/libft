ba16851f414764004e943cf2a1ccb107d0c0e308
El Mehdi Labiad <ellabiad@c1r1p5.42nice.fr>
2025-04-01 11:00:10 +0200

    added more function of the first part, doing now strlcpy

--- ft_memmove.c
+++ ft_memmove.c
@@ -1,28 +1,35 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ellabiad <ellabiad@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/03/31 14:57:47 by ellabiad          #+#    #+#             */
-/*   Updated: 2025/04/01 10:33:05 by ellabiad         ###   ########.fr       */
+/*   Created: 2025/04/01 10:27:31 by ellabiad          #+#    #+#             */
+/*   Updated: 2025/04/01 10:58:57 by ellabiad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void    *ft_memcpy(void *dest, const void *src, size_t n)
+void    *ft_memmove(void *dest, const void *src, size_t n)
 {
-    unsigned char   *p_src;
-    unsigned char   *p_dest;
+    unsigned char    *p_dest;
+    unsigned char    *p_src;
 
-    p_src = (unsigned char *)src;
     p_dest = (unsigned char *)dest;
-    while (n > 0)
+    p_src = (unsigned char *)src;
+    if (dest > src)
     {
-        *p_dest = *p_src;
-        p_dest++;
-        p_src++;
-        n--;
+        p_dest = p_dest + (n - 1);
+        p_src = p_src + (n - 1);
+        while (n > 0)
+        {
+            *p_dest = *p_src;
+            p_dest--;
+            p_src--;
+            n--;
+        }
     }
+    else if (dest != src)
+        ft_memcpy(dest, src, n);
     return (dest);
 }
