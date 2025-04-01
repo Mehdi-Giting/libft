bd9c03c6ed20acc4ac708b405525dfd6ea75b484
El Mehdi Labiad <ellabiad@c1r1p5.42nice.fr>
2025-04-01 18:43:28 +0200

    working on strlcat

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,27 +6,20 @@
 /*   By: ellabiad <ellabiad@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/01 12:25:36 by ellabiad          #+#    #+#             */
-/*   Updated: 2025/04/01 17:56:59 by ellabiad         ###   ########.fr       */
+/*   Updated: 2025/04/01 18:43:19 by ellabiad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 size_t  ft_strlcat(char *dest, const char *src, size_t size)
 {
-    size_t  dest_index;
-    size_t  src_index;
-    size_t  len_dest;
+    size_t  src_len;
+    size_t  dest_len;
 
-    dest_index = ft_strlen(dest);
+    src_len = ft_strlen(src);
     dest_len = ft_strlen(dest);
-    src_index = 0;
-    if (size <= dest_index)
-        return (ft_strlen(src) + size);
-    while (dest_index < size - 1 && src[src_index] != '\0')
-    {
-        dest[dest_index] = src[src_index];
-        dest_index++;
-        src_index++;
-    }
-    dest[dest_index] = '\0';
-    return (ft_strlen(src) + dest_index);
+    if (dest_len >= size)
+        return (src_len + dest_len);
+    if ()
+    dest[] = '\0';
+    return ();
 }
